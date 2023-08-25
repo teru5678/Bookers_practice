c0a268517a5d3a01824425a97c42eea0efc4af0e
teru5678 <toyoteru0719@gmail.com>
2023-08-25 19:59:42 +0900

    bookコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
   devise_for :users
-  get 'homes/top' => 'homes#top'
+  root to:'homes#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
